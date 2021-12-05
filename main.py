a5f8e00f03acd269cae31c6c0a874b3bed81ae3d
Yingning Chen <sc19yc@leeds.ac.uk>
2021-12-05 16:25:25 +0000

    finish random creating data

--- main.py
+++ main.py
@@ -5,12 +5,26 @@ import time
 
 
 def create_data():
-    number_list = []
-    for i in range(1, 21):
-        number_list.append(i)
-    number_list.append(None)
-    data_list = [[number_list[random.randint(0, 20)] for j in range(1, 11)] for i in range(1, 11)]
+
+    # 10 group data / 10 value per group
+    # range of number of x from 10 to 25
+    data_list = [x_random() for i in range(1, 11)]
     return data_list
+
+def x_random():
+    num_x = random.randint(10,26)
+    x_list = []
+    # randomly arrange the range of x
+    for i in range(0,num_x+1):
+        x_list.append(random.randint(1,21));
+    num_random = random.randint(1,5)
+    # randomly give graph data None
+    for i in range(1,num_random+1):
+        chose_x = random.randint(0,num_x)
+        while(x_list[chose_x] is None):
+            chose_x = random.randint(0,num_x)
+        x_list[chose_x] = None
+    return x_list
 
 
 def save_data_as_csv():
@@ -40,13 +54,15 @@ def create_plot_chart(data_list):
         plt.clf()
         plt.pause(1)
 
+# get the answer from the participants and
+def answer_question(answer):
 
-def answer_question():
     pass
 
 
 if __name__ == '__main__':
     data_list = create_data()
+    print(data_list)
     plt.ion()
     create_line_chart(data_list)
     create_plot_chart(data_list)
